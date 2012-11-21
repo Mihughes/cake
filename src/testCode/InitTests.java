ecc25ccdfd489140e57f9ba1b282748ab62c9a72
Colton Deskin <cdeskin@mymail.mines.edu>
2012-11-20 17:24:49 -0700

    Adding gitignore and test class

---
+++ src/testCode/InitTests.java
@@ -0,0 +1,26 @@
+package testCode;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import mainCode.Ball;
+import mainCode.Bullet;
+import mainCode.Cake;
+import mainCode.Canon;
+import mainCode.ControlPanel;
+import mainCode.SaveTheCake;
+
+public class InitTests {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
